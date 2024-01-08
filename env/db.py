24d78b4474d4204c371e532bfbd50f3637ab982a
DuduRamone <eduravaresramone@gmail.com>
2024-01-08 11:32:42 -0300

    add: database connection

---
+++ env/db.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
+
+client = MongoClient("mongodb+srv://teste:teste@chatapp.8xqzhwv.mongodb.net/")
+
+chat_db = client.get_database("ChatDB")
+users_collection = chat_db.get_collection("users")
+
+def save_user(username, email, password):
+  password_hash = generate_password_hash(password)
+  users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
+
+save_user("dudu", "dudu@mail.com", "dudududu")
